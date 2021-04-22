d87f61b88bdfa704f473c30c340b51118cead0ef
bkxadapper <kritsada1204@gmail.com>
2021-04-23 03:21:44 +0700

    add login template

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>multi role php & pdo </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <form action="login_db.php" method="post" class="form-horizontal my-5">
+        <label for="email" class="col-sm-3 control-label">Email</label>
+        <div class="col-sm-12">
+            <input type="text" name="txt_email" class="form-control" require placeholder="enter email">
+        </div>
+        </form>
+    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
